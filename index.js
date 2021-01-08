6801173ca6f658611edc3afe0362dd15e4bedc2f
adriabama06 <68083226+adriabama06@users.noreply.github.com>
2021-01-08 18:20:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const ytdl = require('ytdl-core');
-const { token, prefix } = require('./config.json');
+const prefix = require('./config.json').prefix;
+const token = require('./config.json').token;
 const ffmpeg = require("ffmpeg-static");
 
 
